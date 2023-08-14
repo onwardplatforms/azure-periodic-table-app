e31bf0b9e69719453bf3782ee494e9da32b53afb
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2023-08-13 22:22:56 -0400

    Maybe

--- src/app/resource/[id]/page.tsx
+++ src/app/resource/[id]/page.tsx
@@ -5,15 +5,21 @@ import path from 'path';
 
 // get url param
 export default function Page({ params }: { params: { id: string } }) {
-  const filePath = `/code/${params.id}.tf`;
+  const filePath = path.join(
+    process.cwd(),
+    'public',
+    'code',
+    `${params.id}.tf`
+  );
   let codeSnippet = '';
 
   try {
     // this file may or may not be here, so we need to handle the error
 
     codeSnippet = fs.readFileSync(filePath, 'utf8');
+    console.log(codeSnippet);
   } catch (err) {
-    console.log(err);
+    console.log('nope');
     // Handle the error appropriately, e.g., return an error component or message.
   }
 
