1b5cde1efad5479b7f5248610eeb4b80469a3d68
Justin O'Connor <jsoconno@gmail.com>
2023-08-14 15:15:58 -0400

    try to introduce a new change

--- public/code/anomaly-detector-service.tf
+++ public/code/anomaly-detector-service.tf
@@ -2,7 +2,7 @@ resource "azurerm_cognitive_account" "main" {
   name                = "ad-${local.naming_suffix}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  kind                = "Face"
+  kind                = "OpenAI"
 
   sku_name = "S0"
 
