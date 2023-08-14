d5e81b1739eca0807d655cf0be0a1230846ac7ef
Justin O'Connor <jsoconno@gmail.com>
2023-08-14 10:29:02 -0400

    push to repo for webhook testing

--- python/code-collector.py
+++ python/code-collector.py
@@ -12,7 +12,7 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
 
-# Step 1: Parse the TSX file and extract all terraformUrl values.
+# Step 1: Parse the TSX file and extract all terraformUrl values...
 
 with open('../src/app/data.ts', 'r') as f:
     tsx_content = f.read()
