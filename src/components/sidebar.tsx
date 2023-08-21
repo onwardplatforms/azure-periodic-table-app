cfd4371c4ebfa8aaaeaabbdb1f5079676438dcac
Justin O'Connor <jsoconno@gmail.com>
2023-08-21 10:35:05 -0400

    remove commented code

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -94,56 +94,8 @@ export default function Sidebar({
               <div className="flex">
                 <h1 className="font-bold text-2xl">{activeElement.name}</h1>
               </div>
-              {/* <div className="font-light">
-                <CopyBox
-                  text={`${activeElement?.resource}/${activeElement?.entity}`}
-                />
-              </div> */}
             </div>
           </SheetTitle>
-          {/* <div className="mb-4">
-            <p className="text-left break-words w-full mb-4">
-              {activeElement?.description}
-            </p>
-          </div>
-          <div
-            className={`flex flex-col justify-center items-start py-1 px-2 lg:mx-0 w-fit rounded my-1 ${categoryData.find(
-              (item) => item.name === activeElement.category
-            )?.color
-              }`}
-          >{activeElement.category}</div>
-          <div className="flex flex-col justify-center items-start my-6">
-            <div className="flex justify-center items-center">
-              <span className="mr-4">
-                {activeElement?.learnUrl && (
-                  <a
-                    target="_blank"
-                    href={activeElement?.learnUrl}
-                    className="flex justify-start items-center text-sm break-all border p-2 rounded-lg border-gray-500 hover:border-gray-200 transition-all"
-                  >
-                    <div className="mr-2">
-                      <Icons.Microsoft width={16} height={16} />
-                    </div>
-                    <span>{isMobile ? 'Learn' : 'Microsoft Learn'}</span>
-                  </a>
-                )}
-              </span>
-              <span className="mr-4">
-                {activeElement?.pricingReferenceUrl && (
-                  <a
-                    target="_blank"
-                    href={activeElement?.pricingReferenceUrl}
-                    className="flex justify-start items-center text-sm break-all border p-2 rounded-lg border-gray-500 hover:border-gray-200 transition-all"
-                  >
-                    <div className="mr-2">
-                      <Icons.Microsoft width={16} height={16} />
-                    </div>
-                    <span>{isMobile ? 'Cost' : 'Resource Cost'}</span>
-                  </a>
-                )}
-              </span>
-            </div>
-          </div> */}
           <div className="my-6 text-left">
             <Card className="w-[100%]">
               <CardHeader>
