975b9802dbf297bfd0d0a2c2dbbb1e4daba72d04
moabukar <mo-abukar10@hotmail.com>
2023-09-30 02:13:56 +0100

    add databases + storage resources

--- public/azure/code/terraform/management-group.tf
+++ public/azure/code/terraform/management-group.tf
@@ -3,18 +3,18 @@ data "azurerm_subscription" "current" {
 
 resource "azurerm_management_group" "example_parent" {
   display_name = "ParentGroup"
-  
+
   subscription_ids = [
-  data.azurerm_subscription.current.subscription_id,
+    data.azurerm_subscription.current.subscription_id,
   ]
 }
 
 resource "azurerm_management_group" "example_child" {
   display_name               = "ChildGroup"
   parent_management_group_id = azurerm_management_group.main_parent.id
-  
+
   subscription_ids = [
-  data.azurerm_subscription.current.subscription_id,
+    data.azurerm_subscription.current.subscription_id,
   ]
   # other subscription IDs can go here
 }
